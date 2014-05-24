398e7172310f24637b8e800c4aa1b51fed137160
jreesuk <jreesuk@users.noreply.github.com>
2014-05-24 20:05:50 +0100

    Minor tidying up

--- src/entropizer.js
+++ src/entropizer.js
@@ -1,14 +1,14 @@
 (function() {
 
-	var defaultClasses = ['lowercase', 'uppercase', 'numeric', 'symbolsCommon', 'symbolsUncommon'];
-	var defaultSymbolsCommon = ' ,.?!';
-	var defaultSymbolsUncommon = '"£$%^&*()-_=+[]{};:\'@#~<>/\\|`¬¦';
+	var defaultClasses = ['lowercase', 'uppercase', 'numeric', 'symbolsCommon', 'symbolsUncommon'],
+		symbolsCommon = ' ,.?!',
+		symbolsUncommon = '"£$%^&*()-_=+[]{};:\'@#~<>/\\|`¬¦';
 
 	// Constructor
 	var Entropizer = function(options) {
 		var classes = (options && options.classes) || defaultClasses;
 		this.classes = [];
-		for(var i = 0; i < classes.length; i++) {
+		for (var i = 0; i < classes.length; i++) {
 			this.classes.push(typeof classes[i] === 'string' ? Entropizer.classes[classes[i]] : classes[i]);
 		}
 	};
@@ -18,47 +18,48 @@
 		lowercase: { regex: /[a-z]/, size: 26 },
 		uppercase: { regex: /[A-Z]/, size: 26 },
 		numeric: { regex: /[0-9]/, size: 10 },
-		symbols: { characters: defaultSymbolsCommon + defaultSymbolsUncommon },
-		symbolsCommon: { characters: defaultSymbolsCommon },
-		symbolsUncommon: { characters: defaultSymbolsUncommon },
+		symbols: { characters: symbolsCommon + symbolsUncommon },
+		symbolsCommon: { characters: symbolsCommon },
+		symbolsUncommon: { characters: symbolsUncommon },
 		hexadecimal: { regex: /[a-fA-F0-9]/, size: 16 }
 	};
 
-	Entropizer.prototype.evaluate = function(password) {
+	// Find the contribution of a character class to a password's alphabet
+	Entropizer.prototype._evaluateClass = function(charClass, password) {
+		var chars, i;
+		if (charClass.regex && charClass.regex.test(password)) {
+			return charClass.size;
+		}
+		else if (charClass.characters) {
+			chars = charClass.characters;
+			for (i = 0; i < chars.length; i++) {
+				if (password.indexOf(chars[i]) > -1) {
+					return chars.length;
+				}
+			}
+		}
+		return 0;
+	};
 
-		// Null or empty
+	// Calculate the number of bits of entropy in a password
+	Entropizer.prototype.evaluate = function(password) {
+		var i, alphabetSize = 0;
+
 		if (!password) {
 			return 0;
 		}
 		
-		var currentClass, i, j, charsetSize = 0;
-
-		// Find the size of the union of all the character classes used in the password
+		// Find the alphabet of the password (the union of all the classes it uses)
 		for (i = 0; i < this.classes.length; i++) {
-			currentClass = this.classes[i];
-
-			// Regex-based
-			if (currentClass.regex && currentClass.regex.test(password)) {
-				charsetSize += currentClass.size;
-			}
-			// Characters-based
-			else if (currentClass.characters) {
-				for (j = 0; j < currentClass.characters.length; j++) {
-					if (password.indexOf(currentClass.characters[j]) > -1) {
-						charsetSize += currentClass.characters.length;
-						break;
-					}
-				}
-			}
+			alphabetSize += this._evaluateClass(this.classes[i], password);
 		}
 		
 		// If it's all unknown characters, return 0 instead of -Infinity
-		if (charsetSize === 0) {
+		if (alphabetSize === 0) {
 			return 0;
 		}
 
-		// Bits per character is log_2 of character set size
-		return Math.log(charsetSize) / Math.log(2) * password.length;
+		return Math.log(alphabetSize) / Math.log(2) * password.length;
 	};
 
 	// Define AMD module
@@ -67,7 +68,7 @@
 			return Entropizer;
 		});
 	}
-	// Define global
+	// Define global if no AMD
 	else if (typeof window === 'object') {
 		window.Entropizer = Entropizer;
 	}
